6f595ce330423ab5503f489ee5d64f31b02731fd
Rahul Kumar Jain <62795381+jainrahul11@users.noreply.github.com>
2023-01-17 10:17:20 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                <strong>ScrapZen</strong>              
             </h6>
             <p>
-            Book-IT is an online React web application where the customer can purchase books online.
+            ScrapZen is an online React web application where the customer can purchase books online.
              Through this book store the users can search for a book by its title and
              later can add to the shopping cart and finally purchase using credit card transaction.
             </p>
